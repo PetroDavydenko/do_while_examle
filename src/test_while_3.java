d1eaba7c43b12d00ba21b2cc119afb99971138c2
Admin <petrodavidenko32@gmail.com>
2022-03-21 23:59:32 +0200

    Initial commit

---
+++ src/test_while_3.java
@@ -0,0 +1,10 @@
+public class test_while_3 {
+    public static void main(String[] args) {
+        int a = 1;
+        do {
+            a++;
+            System.out.println(a + " ");
+        }
+        while (a < 0);
+    }
+}
